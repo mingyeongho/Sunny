8b14fefe30af8bab7ac2d72b1c99567bd30cb05f
민경호 <1736s@naver.com>
2024-06-08 12:24:50 +0900

    feat: 스토리북 decorator 설정

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,8 +1,14 @@
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 import { GlobalStyle } from "../src/styles/globalStyle";
+import { Global } from "@emotion/react";
+import React from "react";
+
+const GlobalStyles = () => {
+  return <Global styles={GlobalStyle} />;
+};
 
 export const decorators = [
   withThemeFromJSXProvider({
-    GlobalStyles: GlobalStyle,
+    GlobalStyles,
   }),
 ];
